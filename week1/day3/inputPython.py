5ff511342b275a50309fa34df55361587e9ae5bf
randy chong <randychong27@yahoo.com>
2021-03-10 11:53:21 -0600

    cleaned up code

--- week1/day3/inputPython.py
+++ week1/day3/inputPython.py
@@ -1,4 +1,4 @@
 #name_of_user = input ("What is your name?")
-nameOfUser = input ("What is your first name?")
-lastNameOfUser = input ("What is your last name?")
+nameOfUser = input("What is your first name?")
+lastNameOfUser = input("What is your last name?")
 print("Hello %s %s, welcome to python" % (nameOfUser, lastNameOfUser))
