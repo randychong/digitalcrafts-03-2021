a6156eeb20e2e24ed87f6bb2171ddf5098a8d46a
randy chong <randychong27@yahoo.com>
2021-03-10 10:25:11 -0600

    updated bash profile, made assignments.md and intro.md, made python function "Hello World"

---
+++ week1/day3/pythonExamples.py
@@ -0,0 +1,24 @@
+# Variable
+# nameOfVariable = assignedValueToVariable
+# String === "", "word", "1", "functionName"
+greeting = "Hello World"
+# Integer === whole number, 1, 23, 934234, 9392
+classSize = 23
+# Float === 4.5, 4.587867, etc.
+floatExample = 4.53
+# Boolean === true, false
+sumOfNumbers = 1+1
+# Name, aassignement operator (=), value that I am assigning
+variableName = True
+# List, seqquence of values followed by [value1,value2, etc]
+# [1, 2, 3]
+# ["string", "string2", "blahblahblah"]
+# [False, True, True]
+# ["string", 1, 3.4, True, "string"]
+# nesting
+# ["list", ["list"]]
+# Dictionary
+# Sequence of labeled values {key: value}
+# {"stringName": "StringValue"}
+#{"FavoriteColor": "Red"}
+print("Hello World")
