deb1d1dd027202a922aa5a972b90e71159223372
randy chong <randychong27@yahoo.com>
2021-03-11 12:57:31 -0600

    created lists and dicitonary.pys

---
+++ week1/day4/howToAccessLists.py
@@ -0,0 +1,16 @@
+# a list
+pets = ["doggo", "cat", "iguana"]
+# how to access list
+dog = pets[0]
+# print(dog)
+# index starts at 0
+# example: doggo is 0, cat is 1, and iguana 2
+
+# negative index will access starting at end of the list
+# demonSpawn = pets[-1]
+# print(demonSpawn)
+# myFavoriteTwo = pets[0:2]
+# print(myFavoriteTwo)
+
+# entire list
+print(pets)
---
+++ week1/day4/listTuplesDictionaries.py
@@ -0,0 +1,22 @@
+# Lists
+# Contains stuff in square brackets
+aList = ["list item", 2, 3 "four"]
+
+#Dictionary
+#Curly brackets
+#Sequence of labeled values
+dictionaryOfUsers = {name: "Joe", age: "???", height: "6-1", hairHeight: "???"}
+
+user ={
+    "name": "Joe",
+    "placesLived": {
+        "US": {"city": "tallanasty"},
+        "Overseas": {"Asia": "Philippines"},
+    },
+    "favoriteFoods": ["spaghetti", "chicken", "sushi", "lumpia"]
+}
+
+#Tuples
+#Parentheses()
+
+tupleExample = (33, 44)
