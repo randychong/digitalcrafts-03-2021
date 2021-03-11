81daa40bf8871505a03e1de1c842ffa32b8386a7
randy chong <randychong27@yahoo.com>
2021-03-11 16:11:55 -0600

    updated calc.py with functions and created todo.py

--- week1/day3/day3-calc.py
+++ week1/day3/day3-calc.py
@@ -1,15 +1,48 @@
-num1 = int(input("Select a number "))
-op = input("Select an operator: (+, -, *, / ")
-num2 = int(input("Select another number "))
+quitMessage = print("Press q at anytime to close the calculator.")
+num1 = int(input("Select a number: "))
+op = input("Select an operator: (+, -, *, /: ")
+num2 = int(input("Select another number: "))
+currentInput = num1, op, num2
+def closeCalc (q):
+    return exit()
+
+def add(num1, num2):
+    add = (num1 + num2)
+    return print(add)
+
+def subtract(num1, num2):
+    subtract = (num1 - num2)
+    return print(subtract)
+
+def multiply(num1, num2):
+    multiply = (num1 * num2)
+    return print (multiply)
+
+def divide(num1, num2):
+    divide = (num1 / num2)
+    return print (divide)
 
 if op == "+":
-    print(num1 + num2)
+    add(num1, num2)
 elif op == "-":
-    print(num1 - num2)
+    subtract(num1, num2)
 elif op == "*":
-    print(num1 * num2)
+    multiply(num1, num2)
 elif op == "/":
-    print(num1 / num2)
+    divide(num1, num2)
+
+
+
+
+
+# if op == "+":
+#     print(num1 + num2)
+# elif op == "-":
+#     print(num1 - num2)
+# elif op == "*":
+#     print(num1 * num2)
+# elif op == "/":
+#     print(num1 / num2)
 # test code below
 # try:
 #     print(num1 + num2)
---
+++ week1/day4/todo.py
@@ -0,0 +1,7 @@
+add = input("Press 1 to add a task.")
+delete = input("Press 2 to delete a task.")
+view = input("Press 3 to view a task.")
+quit = input("Press q to quit")
+
+def question (add, delete, view, quit):
+question(add, delete, view, quit):
