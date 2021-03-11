bc8a9d1aac25eeff7936ed7715d3123f19752f41
randy chong <randychong27@yahoo.com>
2021-03-11 11:15:40 -0600

    updated day4 class notes

---
+++ week1/day4/day4.notes.py
@@ -0,0 +1,30 @@
+# defining a function
+# the word "def", the name of the function, parentheses (), then a colon :, followed by the body
+# in the body, you need a return statement
+
+# def printMessage():
+#     return print("hai")
+
+# def add(number1, number2):
+#     return number1 + number2
+
+# def multiply(number1, number2):
+#     return number1 * number2
+
+# calling or invoking a function
+# printMessage()
+
+#print(add(2, 3))
+
+#function that prints a string
+string1 = input("Enter a word ")
+string2 = input("Enter another word ")
+
+def input1 (string1, string2):
+    return print(string1 + string2)
+
+input1(string1, string2)
+
+# SCOPE - what does the computer/program have access to
+# SCOPE is about having access to certain things in your program
+# Global scope means anyone can access this variable, function from anywhere inside your program
