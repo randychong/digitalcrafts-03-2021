6ff3cd343dccdcf1bf580c301ae9df7eef923512
randy chong <randychong27@yahoo.com>
2021-03-12 08:36:06 -0600

    made todo app that allows user to add task, delete task, view task, or quit app

--- week1/day4/new.todo.app.py
+++ week1/day4/new.todo.app.py
@@ -1,37 +1,63 @@
 tasks = []
-
 # defining the menu
-mainMenu = """
-Choose an action:\n
+# mainMenu = """
+# Choose an action:\n
+# Press 1 to add a task
+# Press 2 to delete a task
+# Press 3 to view all tasks
+# Press q to quit
+# """
+def mainMenu (): 
+    mainMenu = print("""
+Choose an action:"\n
 Press 1 to add a task
 Press 2 to delete a task
 Press 3 to view all tasks
 Press q to quit
-"""
+""")
+
+mainMenu()
+
 # defining choice
 def quitApp():
     quitMessage = input("Would you like to quit? (y)yes (n) no\n")
     if quitMessage == "y":
         exit()
+    else:
+        mainMenu()
 
 choice = input(mainMenu)
 
 while len(choice) > 0:
     if choice == "1":
         title = input("What task do you need to complete?\n")
-        if len(title) > 0:
-            tasks.append(title)
         priority = input("Is this task's priority high, medium, or low?\n")
-        if len(priority) > 0:
-            tasks.append(priority)
+        # if len(title) > 0:
+        #     tasks.append(title)
+        dictionaryOfTasks = {title, priority}
+        if len(dictionaryOfTasks) > 0:
+            tasks.extend(dictionaryOfTasks)
+        # if len(priority) > 0:
+        #     tasks.append(priority)
         print("Here is your updated to-do list:")
-        for task in tasks:
-            print("%s" % (task))
+        count = 1
+        for task in dictionaryOfTasks:
+            print("%s: %s" % (title, priority))
+            count += 1
+        quitApp()
+    elif choice == "2":
+        choice2 = input("Which task would you like to delete?\n")
+        dictionaryOfTasks = {}
+        for task in dictionaryOfTasks:
+            print("%s: %s" % (title, priority))
+            count += 1
         quitApp()
     elif choice == "3":
         count = 1
         for task in tasks:
             print("%d: %s" % (count, task))
+            count +=1
+        quitApp()
     elif choice == "q":
         quitApp()
     # else:
