ebe44aa773bcdc8f75a6a654096c242a93f9c341
randy chong <randychong27@yahoo.com>
2021-03-17 01:40:58 -0500

    practice more with classes

--- week2/day2/buildingclass.py
+++ week2/day2/buildingclass.py
@@ -12,14 +12,21 @@ class building:
         self.height = height
         self.capacity = capacity
         self.area = area
+        self.type = "commercial"
 
-    # def build_specs = print(building)
+    def build_specs(self):
+        print(f" building height: {self.height} \n building capacity: {self.capacity} \n building area in sq ft: {self.area} \n building type: {self.type}")
 
-def building_creation():
-    building_name = ""
-    build_height = input("Please enter the building's height: \n")
-    build_capacity = input("Please enter the building's capacity: \n")
-    build_area = input("Please enter the building's area in square feet: \n")
-    return print("height: ", build_height, "capacity: ", build_capacity, "area: ", build_area,"sq ft")
+def build_creation():
+    new_build = ""
+    build_height = input("Please enter the building's height:\n")
+    build_capacity = input("Please enter the building'scapacity: \n")
+    build_area = input("Please enter the building's area insquare feet: \n")
+    new_build = building(build_height, build_capacity, build_area)
+    building.build_specs(new_build)
+    # return print("height: ", build_height, " capacity: ",build_capacity, " area: ", build_area,"sq ft")
 
-building_creation()
+count = 0
+while count < 5:
+    build_creation()
+    count += 1
