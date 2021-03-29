ccf3e29c17882f0604d5f6698d66bae49109a571
randy chong <randychong27@yahoo.com>
2021-03-29 14:41:53 -0500

    created functions for each operand

---
+++ week4/day1/javascript.calculator.js
@@ -0,0 +1,28 @@
+//add function
+//subtract function
+//multiply function
+//divide function
+
+// function add(num1, num2){
+//     return console.log(num1 + num2)
+// }
+
+// add (1, 2)
+
+// function subtract(num1, num2){
+//     return console.log(num1 - num2)
+// }
+
+// subtract(1, 2)
+
+// function multiply(num1, num2){
+//     return console.log(num1 * num2)
+// }
+
+// multiply (1, 2)
+
+// function divide(num1, num2){
+//     return console.log(num1 / num2)
+// }
+
+// divide (1, 2)
