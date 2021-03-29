9e5c04d8a708b273fec997120b903056ae8ab9e7
randy chong <randychong27@yahoo.com>
2021-03-29 11:37:58 -0500

    javascript practice assignment

---
+++ week4/day1/javascript.js
@@ -0,0 +1,46 @@
+let snack = "seaweed"
+let food = "musubi"
+let store = "H-Mart"
+console.log(snack)
+console.log(food)
+console.log(store)
+
+let array1 = ["seaweed", "musubi", "H-mart"]
+console.log(array1)
+
+let drink = "water"
+console.log(drink)
+
+let number = 3
+console.log(number)
+
+let string = "string_object"
+string = "This is a new string"
+let multiply = number * 5
+console.log(multiply)
+
+let array2 = ["swag", 123123, true, false, "Boolean"]
+console.log(array2)
+console.log(array2[1])
+console.log(array2[3])
+console.log(array2[4])
+console.log(array2.length)
+
+let batman = {name:"Batman", age:20, height:"7.1", metric: "ft"}
+console.log(batman)
+
+let chuck = {name:"Chuck Norris", age:35, height:"9.9"}
+let bruce = {name:"Bruce Lee", age:33, height:"7.5"}
+let goku = {name:"Goku", age:"ageless", height:"over 9000"}
+let mary = {name:"Mary Poppins", age:"??", height:"too high"}
+let mom = {name:"My mom", age:"none-ya-business", height:"also-none-ya-business"}
+
+console.log(chuck.name)
+console.log(mom.age)
+console.log(goku.height)
+goku.height = 1.2
+console.log(goku.height)
+goku.metric = "meters"
+console.log(goku.metric)
+bruce.sport = "competitive seaweed eating"
+console.log(bruce.sport)
