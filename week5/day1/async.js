7bb1bc32699713acce77900993372ddc442ccf82
randy chong <randychong27@yahoo.com>
2021-04-05 11:53:26 -0500

    linked to weather api

--- week5/day1/async.js
+++ week5/day1/async.js
@@ -1,4 +1,6 @@
 const getWeatherData = async ()=> {
-    const data = await fetch(api.openweathermap.org/data/2.5/weather?q=Houston&appid=622d85fb1a656b0a3ab04b6a4f7fe706)
+    const data = await fetch("http://api.openweathermap.org/data/2.5/weather?q=houston&appid=622d85fb1a656b0a3ab04b6a4f7fe706")
     const formattedJson = await data.json()
+    console.log(formattedJson)
 }
+getWeatherData()
