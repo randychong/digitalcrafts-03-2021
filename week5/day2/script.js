1a7578300ad5917f6cc0261e055b48d7ed9c61cf
randy chong <randychong27@yahoo.com>
2021-04-06 14:49:06 -0500

    added birthday info with script

--- week5/day2/script.js
+++ week5/day2/script.js
@@ -1,6 +1,6 @@
 const getPeopleData = async () => {
 
-    let peopleData = await fetch("https://fakerapi.it/api/v1/persons?_quantity=30");
+    let peopleData = await fetch("https://fakerapi.it/api/v1/persons?_quantity=5");
     let json = await peopleData.json();
     console.log(json)
 
@@ -8,11 +8,15 @@ const getPeopleData = async () => {
     for (let person of json.data) {
         console.log(person);
         const people = document.createElement("p");
+        people.className = "people-name";
         people.innerHTML = person.firstname + " " + person.lastname;
-        const peopleDiv = document.querySelector(".people-info")
-        peopleDiv.append(people)
-        counter += 1
+        const birthday = document.createElement("p");
+        birthday.className = "birthday";
+        birthday.innerHTML = person.birthday
+        const peopleDiv = document.querySelector(".people-container");
+        peopleDiv.append(people, birthday);
+        counter += 1;
     }
     return json;
 }
-getPeopleData()
+getPeopleData();
