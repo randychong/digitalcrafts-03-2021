c6f1284dd875e1023dc9d512d68247805b317477
randy chong <randychong27@yahoo.com>
2021-04-06 14:59:51 -0500

    changed place to company for all elements

--- week5/day2/script.js
+++ week5/day2/script.js
@@ -2,11 +2,9 @@ const getPeopleData = async () => {
 
     let peopleData = await fetch("https://fakerapi.it/api/v1/persons?_quantity=5");
     let json = await peopleData.json();
-    console.log(json)
 
     let counter = 1;
     for (let person of json.data) {
-        console.log(person);
         const people = document.createElement("p");
         people.className = "people-name";
         people.innerHTML = person.firstname + " " + person.lastname;
@@ -19,4 +17,13 @@ const getPeopleData = async () => {
     }
     return json;
 }
-getPeopleData();
+
+const searchButton = document.querySelector(".search-people");
+searchButton.addEventListener("click", () => getPeopleData())
+
+const getCompanyData = async () => {
+
+    let companyData = await fetch("https://fakerapi.it/api/v1/companies?_quantity=5");
+    let json = await companyData.json();
+    console.log(json)
+}
