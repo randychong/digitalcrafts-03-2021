12f5fd63aea700aebea846f4906d3c9d883f34e6
randy chong <randychong27@yahoo.com>
2021-04-06 23:09:06 -0500

    added semicolons after each line of code

--- week5/day1/script.js
+++ week5/day1/script.js
@@ -1,24 +1,28 @@
+const currentDate = document.querySelector(".current-date");
+
+
+
 const getWeatherData = async ()=> {
 
-    const zip = document.querySelector(".zipInput").value
-    const data = await fetch(`http://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=622d85fb1a656b0a3ab04b6a4f7fe706&units=imperial`)
-    const formattedJson = await data.json()
+    const zip = document.querySelector(".zipInput").value;
+    const data = await fetch(`http://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=622d85fb1a656b0a3ab04b6a4f7fe706&units=imperial`);
+    const formattedJson = await data.json();
 
-    const city = document.querySelector(".location")
-    city.innerHTML = formattedJson.name
+    const city = document.querySelector(".location");
+    city.innerHTML = formattedJson.name;
 
-    const temp = document.querySelector(".temperature")
-    temp.innerHTML = formattedJson.main.temp + " °F"
+    const temp = document.querySelector(".temperature");
+    temp.innerHTML = formattedJson.main.temp + " °F";
 
-    const maxTemp = document.querySelector(".maxTemp")
-    maxTemp.innerHTML = formattedJson.main.temp_max + " °F"
+    const maxTemp = document.querySelector(".maxTemp");
+    maxTemp.innerHTML = formattedJson.main.temp_max + " °F";
 
-    const minTemp = document.querySelector(".minTemp")
-    minTemp.innerHTML = formattedJson.main.temp_min + " °F"
+    const minTemp = document.querySelector(".minTemp");
+    minTemp.innerHTML = formattedJson.main.temp_min + " °F";
 
-    const humidity = document.querySelector(".humidity")
-    humidity.innerHTML = formattedJson.main.humidity + "%"
+    const humidity = document.querySelector(".humidity");
+    humidity.innerHTML = formattedJson.main.humidity + "%";
 }
 
-const submit = document.querySelector(".submit")
-submit.addEventListener("click", ()=> getWeatherData())
+const submit = document.querySelector(".submit");
+submit.addEventListener("click", ()=> getWeatherData());
