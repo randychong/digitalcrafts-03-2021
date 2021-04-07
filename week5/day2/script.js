0b00725d24d662973c4163946702b7c3ad4d2c18
randy chong <randychong27@yahoo.com>
2021-04-07 13:56:14 -0500

    styled people card and company card

--- week5/day2/script.js
+++ week5/day2/script.js
@@ -42,14 +42,27 @@ const getCompanyData = async () => {
 
     let counter = 1;
     for (let company of json2.data) {
+        const img2 = document.createElement("img");
+        img2.className = "company-image";
+        img2.height = "160";
+        img2.height = "200";
+        img2.src = `http://placeimg.com/640/480/people/${counter}`
+
         const companies = document.createElement("p");
         companies.className = "companies";
         companies.innerHTML = company.name;
+
         const website = document.createElement("p");
         website.className = "website";
         website.innerHTML = company.website;
+
+        const companyCard = document.createElement("div");
+        companyCard.className = "company-card";
+
         const companyDiv = document.querySelector(".company-container");
-        companyDiv.append(companies, website);
+
+        companyCard.append(img2, companies, website);
+        companyDiv.append(companyCard);
         counter += 1;
     }
 }
