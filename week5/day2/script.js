6aafb7b39d8f4e84be8eff3de2f6acf0034f6c7a
randy chong <randychong27@yahoo.com>
2021-04-07 17:36:42 -0500

    created opaque white background for card

--- week5/day2/script.js
+++ week5/day2/script.js
@@ -22,10 +22,14 @@ const getPeopleData = async () => {
         const peopleCard = document.createElement("div");
         peopleCard.className = "people-card";
 
+        const cardContainer = document.createElement("div")
+        cardContainer.className = "card-container";
+
         const peopleDiv = document.querySelector(".people-container");
 
         peopleCard.append(img, people, email)
-        peopleDiv.append(peopleCard);
+        cardContainer.append(peopleCard)
+        peopleDiv.append(cardContainer);
         counter += 1;
     }
     return json;
