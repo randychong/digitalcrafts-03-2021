e3219b89df7ff00f2c6236a17c24a646c23bcf9e
randy chong <randychong27@yahoo.com>
2021-04-12 11:15:18 -0500

    updated button class to search button

--- week6/front-end-proj/script.js
+++ week6/front-end-proj/script.js
@@ -1,6 +1,8 @@
 const mainContainer = document.querySelector(".main-container")
+
 const getRecipe = async () => {
-    let data = await fetch("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?query=burger", {
+    const search = document.querySelector(".search-input").value
+    let data = await fetch(`https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?query=${search}`, {
         // "headers": {
         //     "x-rapidapi-key": "0eb660dc97msh0b6247972cec6a3p1c8277jsn29d8b3adfdd2",
         //     "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
