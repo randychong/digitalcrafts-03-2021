53455171a2be000493a9ab2c0a64d937da313c05
randy chong <randychong27@yahoo.com>
2021-04-12 11:10:01 -0500

    added header, input field, and button for main page

--- week6/front-end-proj/script.js
+++ week6/front-end-proj/script.js
@@ -1,11 +1,10 @@
 const mainContainer = document.querySelector(".main-container")
-
 const getRecipe = async () => {
     let data = await fetch("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?query=burger", {
-        "headers": {
-            "x-rapidapi-key": "0eb660dc97msh0b6247972cec6a3p1c8277jsn29d8b3adfdd2",
-            "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
-        }
+        // "headers": {
+        //     "x-rapidapi-key": "0eb660dc97msh0b6247972cec6a3p1c8277jsn29d8b3adfdd2",
+        //     "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
+        // }
     })
     let json = await data.json();
     console.log(json);
@@ -19,4 +18,3 @@ const getRecipe = async () => {
         counter += 1;
     }
 }
-getRecipe();
