e794db379c0b39b4a34c66fc008f1c74ef73f403
randy chong <randychong27@yahoo.com>
2021-04-12 13:45:02 -0500

    set display grid on main container and centered items

--- week6/front-end-proj/script.js
+++ week6/front-end-proj/script.js
@@ -4,10 +4,10 @@ const searchButton = document.querySelector(".search-button")
 const getRecipe = async () => {
     const search = document.querySelector(".search-input").value
     let data = await fetch(`https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?query=${search}`, {
-        "headers": {
-            "x-rapidapi-key": "0eb660dc97msh0b6247972cec6a3p1c8277jsn29d8b3adfdd2",
-            "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
-        }
+        // "headers": {
+        //     "x-rapidapi-key": "0eb660dc97msh0b6247972cec6a3p1c8277jsn29d8b3adfdd2",
+        //     "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
+        // }
     })
     let json = await data.json();
     console.log(json);
