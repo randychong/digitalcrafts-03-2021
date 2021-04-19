1e684b248292d415aa105cb170f11dda225d441f
randy chong <randychong27@yahoo.com>
2021-04-19 10:53:45 -0500

    added express

--- week7/day1/index.js
+++ week7/day1/index.js
@@ -1,15 +1,24 @@
 console.log("anyeonghaseyo");
 
 const http = require("http");
+const express = require("express");
+const app = express();
 const address ="127.0.0.1";
 const PORT = 3001;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    res.end("hello");
-})
+// const server = http.createServer((req, res) => {
+//     console.log(req.url);
 
-server.listen(PORT, address, () => {
-    console.log(`server is on ${address}:${PORT}`)
-})
+//     if( req.url ==="/hello-something") {
+//         res.statusCode = 200;
+//         res.end(`${req.url} is where you are at`)
+//     } else {
+//         res.statusCode = 200;
+//         res.setHeader("Content-Type", "text/plain");
+//         res.end("hello");
+//     }
+// })
+
+// server.listen(PORT, address, () => {
+//     console.log(`server is on ${address}:${PORT}`)
+// });
