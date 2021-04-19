c8241da5e192040c45b23bc8637bd06bf8c1e512
randy chong <randychong27@yahoo.com>
2021-04-19 11:04:43 -0500

    created index.html

--- week7/day1/index.js
+++ week7/day1/index.js
@@ -5,6 +5,7 @@ const express = require("express");
 const app = express();
 const address ="127.0.0.1";
 const PORT = 3001;
+const {readFile} = require("fs");
 
 // const server = http.createServer((req, res) => {
 //     console.log(req.url);
@@ -18,6 +19,17 @@ const PORT = 3001;
 //         res.end("hello");
 //     }
 // })
+app.get("/", (req, res) => {
+    // res.end("Hello");
+})
+
+app.get("another place", (req, res) => {
+    // res.end("Hello another place");
+})
+
+app.listen(PORT), () => {
+    console.log(`Your server is listening on ${PORT}`)};
+
 
 // server.listen(PORT, address, () => {
 //     console.log(`server is on ${address}:${PORT}`)
