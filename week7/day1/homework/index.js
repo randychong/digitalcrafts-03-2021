5f96d0fdce077f439f207108d2b50b151e00368e
randy chong <randychong27@yahoo.com>
2021-04-19 19:51:30 -0500

    created ul for navbar

--- week7/day1/homework/index.js
+++ week7/day1/homework/index.js
@@ -10,6 +10,18 @@ app.get("/", (req, res) => {
     });
 });
 
+app.get("/store", (req, res) => {
+    readFile("./store.html", "utf8", (err, html) => {
+        res.send(html);
+    });
+});
+
+app.get("/contact", (req, res) => {
+    readFile("./contact.html", "utf8", (err, html) => {
+        res.send(html);
+    });
+});
+
 app.listen(port, () => {
     console.log(`Your server is being hosted on localhost:${port}`);
 });
