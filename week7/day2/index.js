a4babd20026fa1f1c30919f1cb88b127f420b3ff
randy chong <randychong27@yahoo.com>
2021-04-20 09:57:34 -0500

    created app.listen

---
+++ week7/day2/index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+const port = 3006;
+
+app.listen(port, () => {
+    console.log(`Your server is being hosted on localhost:${port}`)
+})
