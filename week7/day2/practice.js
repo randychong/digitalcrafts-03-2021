55218de3a62132283a636a0ba2fa674ee99d2f62
randy chong <randychong27@yahoo.com>
2021-04-20 14:36:04 -0500

    made templates folder

--- week7/day2/practice.js
+++ week7/day2/practice.js
@@ -2,3 +2,8 @@ const express = require("express");
 const app = require();
 
 const port = 3007;
+
+const es6Renderer = require('express-es6-template-engine');
+app.engine('html', es6Renderer);
+app.set('views', 'templates');
+app.set('view engine', 'html');
