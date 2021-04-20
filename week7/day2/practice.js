593402772179ec064fe946d4cd7a8486dc12620f
randy chong <randychong27@yahoo.com>
2021-04-20 15:10:57 -0500

    created db.js and added array

--- week7/day2/practice.js
+++ week7/day2/practice.js
@@ -1,12 +1,18 @@
-const express = require("express");
-const app = require();
+const http = require('http');
 
-const port = 3007;
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const express = require('express');
+const app = express();
 
 const es6Renderer = require('express-es6-template-engine');
 app.engine('html', es6Renderer);
 app.set('views', 'templates');
 app.set('view engine', 'html');
+
+const server = http.createServer(app);
+const db = require('./db');
 
 app.get('/', (req, res) => {
     res.render('home');
