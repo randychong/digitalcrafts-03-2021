58686fdde2d7d29800a481f0ae10ff49a215d33b
randy chong <randychong27@yahoo.com>
2021-04-20 10:05:36 -0500

    created 4 unique routes on server

--- week7/day2/index.js
+++ week7/day2/index.js
@@ -3,6 +3,26 @@ const app = express();
 
 const port = 3006;
 
+app.get("/", (req, res) => {
+    const message = "Welcome to the homepage"
+    res.send(message)
+})
+
+app.get("/about", (req, res) => {
+    const message = "Welcome to the about page"
+    res.send(message)
+}
+
+app.get("/team", (req, res) => {
+    const message = "Welcome to the team page"
+    res.send(message)
+}
+
+app.get("/FAQ", (req, res) => {
+    const message = "Welcome to the FAQ page"
+    res.send(message)
+}
+
 app.listen(port, () => {
     console.log(`Your server is being hosted on localhost:${port}`);
 });
