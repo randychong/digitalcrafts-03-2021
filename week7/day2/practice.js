253cbbb6900db1049468f147b4f63c9124913852
randy chong <randychong27@yahoo.com>
2021-04-20 14:45:08 -0500

    added app.get and listen

--- week7/day2/practice.js
+++ week7/day2/practice.js
@@ -7,3 +7,11 @@ const es6Renderer = require('express-es6-template-engine');
 app.engine('html', es6Renderer);
 app.set('views', 'templates');
 app.set('view engine', 'html');
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.listen(port, () => {
+    console.log(`Your server is being hosted on localhost:${port}`);
+});
