486e2355efade1a9a6f7909267221588423412b0
randy chong <randychong27@yahoo.com>
2021-04-22 12:14:40 -0500

    created route to add friends

--- week7/day4/server/index.js
+++ week7/day4/server/index.js
@@ -11,6 +11,24 @@ app.get("/", (req, res) => {
     res.send("This is Randy's friendlist :)")
 });
 
+//add a friend
+app.post("/friends", async (req, res) => {
+    try {
+        const {name} = req.body
+
+        const newFriend = await pool.query("INSERT INTO friends (name) VALUES($1)", [name]);
+        
+        res.json(newFriend);
+    } catch (err) {
+        console.log(err.message);
+    }
+});
+
+//view friends
+app.get("/view_friends", async (req, res) => {
+
+});
+
 app.listen(port, () => {
     console.log(`Your server is being hosted on localhost:${port}`)
 });
