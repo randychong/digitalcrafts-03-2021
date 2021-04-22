ebf6f4f2261217626789fea8d8c76a7914b5a260
randy chong <randychong27@yahoo.com>
2021-04-22 11:44:34 -0500

    created app.listen

--- week7/day4/server/index.js
+++ week7/day4/server/index.js
@@ -3,3 +3,10 @@ const app = express();
 const cors = require ("cors");
 const port = process.env.PORT || 3008;
 const pool = require("./db.js");
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(port, () => {
+    console.log(`Your server is being hosted on localhost:${port}`)
+})
