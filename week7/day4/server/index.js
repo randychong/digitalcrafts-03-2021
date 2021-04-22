5033dacfb860e7fb1ab9123f689d93aa46130692
randy chong <randychong27@yahoo.com>
2021-04-22 11:49:10 -0500

    created get request for homepage

--- week7/day4/server/index.js
+++ week7/day4/server/index.js
@@ -7,6 +7,10 @@ const pool = require("./db.js");
 app.use(express.json());
 app.use(cors());
 
+app.get("/", (req, res) => {
+    res.send("This is Randy's friendlist :)")
+});
+
 app.listen(port, () => {
     console.log(`Your server is being hosted on localhost:${port}`)
-})
+});
