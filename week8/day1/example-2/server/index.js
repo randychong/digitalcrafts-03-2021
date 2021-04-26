4ba895d43870c1a0ee840ea74baba1186bfe2335
randy chong <randychong27@yahoo.com>
2021-04-26 11:07:50 -0500

    added middleware

--- week8/day1/example-2/server/index.js
+++ week8/day1/example-2/server/index.js
@@ -3,6 +3,9 @@ const app = express();
 const port = 3010;
 const es6Renderer = require("express-es6-template-engine")
 
+app.engine("html", es6Renderer);
+app.set("views", "template")
+
 app.listen(port, () => {
     console.log(`Your server is being hosted on localhost:${port}`)
 });
