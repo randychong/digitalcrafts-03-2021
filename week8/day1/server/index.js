fcf98f850afc27c21e095bcef215281fbaab5c04
randy chong <randychong27@yahoo.com>
2021-04-26 09:31:33 -0500

    created get request for homepage

--- week8/day1/server/index.js
+++ week8/day1/server/index.js
@@ -5,6 +5,10 @@ const port = process.env.PORT || 3009;
 app.use(express.json());
 app.use(cors());
 
+app.get("/", (req, res) => {
+    res.send("Welcome to Randy's Node Server!")
+});
+
 app.listen(port, () => {
     console.log(`Your server is being hosted on localhost:${port}`)
-})
+});
