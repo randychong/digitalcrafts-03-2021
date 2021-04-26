dbd94adf5c61864da543062b84602d2cb90b28c6
randy chong <randychong27@yahoo.com>
2021-04-26 11:07:02 -0500

    added express template and started node server

---
+++ week8/day1/example-2/server/index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+const port = 3010;
+const es6Renderer = require("express-es6-template-engine")
+
+app.listen(port, () => {
+    console.log(`Your server is being hosted on localhost:${port}`)
+});
