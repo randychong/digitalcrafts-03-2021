971d235c3e342c9ef653237d0f1114e71503631a
randy chong <randychong27@yahoo.com>
2021-04-26 09:29:33 -0500

    created app listener

--- week8/day1/server/index.js
+++ week8/day1/server/index.js
@@ -4,3 +4,7 @@ const port = process.env.PORT || 3009;
 
 app.use(express.json());
 app.use(cors());
+
+app.listen(port, () => {
+    console.log(`Your server is being hosted on localhost:${port}`)
+})
