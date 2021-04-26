406bf908022b5fa525f4dee30726c43141fc540c
randy chong <randychong27@yahoo.com>
2021-04-26 10:21:12 -0500

    created post request to add items to shopping list

--- week8/day1/server/index.js
+++ week8/day1/server/index.js
@@ -12,6 +12,24 @@ app.get("/", (req, res) => {
     res.send("Welcome to Randy's Node Server!")
 });
 
+//add to shopping list
+app.post("/shoppinglist", async (req, res) => {
+    try {
+        const {productName} = req.body;
+        const {quantity} = req.body;
+        const {price} = req.body;
+
+        const newItem = await pool.query("INSERT INTO products (productName, quantity, price) VALUES($1, $2, $3)",
+        [productName, quantity, price]
+        );
+
+        res.json("You've added a new item to your shopping list!")
+
+    } catch(err) {
+        console.error(err);
+    }
+});
+
 app.listen(port, () => {
     console.log(`Your server is being hosted on localhost:${port}`)
 });
