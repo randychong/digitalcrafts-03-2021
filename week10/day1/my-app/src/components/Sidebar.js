e5a474d999b069654bcf1657401315f06d214fdc
randy chong <randychong27@yahoo.com>
2021-05-11 09:01:30 -0500

    added ubuntu font

--- week10/day1/my-app/src/components/Sidebar.js
+++ week10/day1/my-app/src/components/Sidebar.js
@@ -5,10 +5,18 @@ export default class Sidebar extends Component {
     render() {
         return (
             <div className="Sidebar">
-                <p className="SidebarItem">Home</p>
-                <p className="SidebarItem">Projects</p>
-                <p className="SidebarItem">Resume</p>
-                <p className="SidebarItem">Contact</p>
+                <p>
+                <a className="SidebarItem">Home</a>
+                </p>                      
+                <p>
+                <a className="SidebarItem">Projects</a>
+                </p>
+                <p>
+                <a className="SidebarItem">Resume</a>
+                </p>
+                <p>
+                <a className="SidebarItem">Contact</a>
+                </p>
             </div>
         );
     }
