f210aeee4b2cc90800cadd62ec415b2c00580e38
randy chong <randychong27@yahoo.com>
2021-05-17 11:20:11 -0500

    added counter to count.js

--- week11/day1/my-app/src/Count.js
+++ week11/day1/my-app/src/Count.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react'
 
 export default class Count extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 100
+        }
+    }
     render() {
         return (
             <div>
                 <h1>Count.js</h1>
+                <p>You have {this.state.count} dollars.</p>
+                <button onClick={() =>
+                this.setState({
+                    count: this.state.count + 100
+                })
+                }>Make more money</button>
             </div>
         )
     }
