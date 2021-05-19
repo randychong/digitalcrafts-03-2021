5ea45f072c7d4dfa66a6c954d6c21afda1127ac8
randy chong <randychong27@yahoo.com>
2021-05-19 10:02:16 -0500

    added state values for each input

--- week11/day3/my-app/src/App.js
+++ week11/day3/my-app/src/App.js
@@ -1,23 +1,9 @@
-import './App.css';
+import Form from "./components/Form.js"
 
 function App() {
   return (
     <div className="App">
-      <h1>Forms React</h1>
-      <div className="shipping-container">
-      <form className="shipping-form">
-        <input type="text" className="input-item" placeholder="First Name"/>
-        <input type="text" className="input-item" placeholder="Last Name"/>
-        <input type="text" className="input-item" id="street" placeholder="Street Address"/>
-        <input type="text" className="input-item" id="apt" placeholder="Apt, Suite"/>
-        <input type="text" className="input-item" placeholder="Postal Code"/>
-        <input type="text" className="input-item" placeholder="City"/>
-        <input type="text" className="input-item" placeholder="State"/>
-        <input type="text" className="input-item" placeholder="Country"/>
-        <input type="text" className="input-item" placeholder="Phone"/>
-        <input type="submit" className="input-item" id="btn" placeholder="Submit"/>
-      </form>
-      </div>
+      <Form />
     </div>
   );
 }
--- week11/day3/my-app/src/components/Form.js
+++ week11/day3/my-app/src/components/Form.js
@@ -1,10 +1,73 @@
 import React, { Component } from 'react'
+import '../App.css';
 
 export default class Form extends Component {
+    state = {
+        firstName: "",
+        lastName: "",
+        address: "",
+        apt: "",
+        postal: "",
+        city: "",
+        state: "",
+        country: "",
+        phone: "",
+    }
     render() {
         return (
             <div>
-                
+                <h1>Forms React</h1>
+                    <div className="shipping-container">
+                        <form className="shipping-form">
+                            <input type="text"
+                                className="input-item"
+                                placeholder="First Name" 
+                                value={this.state.firstName} 
+                                onChange={(e) =>
+                                    this.setState({
+                                    firstName: e.target.value
+                                    })
+                                }
+                            />
+                            <input type="text" 
+                            className="input-item" 
+                            placeholder="Last Name"
+                            />
+                            <input type="text" 
+                            className="input-item" 
+                            id="street" placeholder="Street Address"
+                            />
+                            <input type="text" 
+                            className="input-item" 
+                            id="apt" placeholder="Apt, Suite"
+                            />
+                            <input type="text" 
+                            className="input-item" 
+                            placeholder="Postal Code"
+                            />
+                            <input type="text" 
+                            className="input-item" 
+                            placeholder="City"
+                            />
+                            <input type="text" 
+                            className="input-item" 
+                            placeholder="State"
+                            />
+                            <input type="text" 
+                            className="input-item" 
+                            placeholder="Country"
+                            />
+                            <input type="text" 
+                            className="input-item" 
+                            placeholder="Phone"
+                            />
+                            <input type="submit" 
+                            className="input-item" 
+                            id="btn" 
+                            placeholder="Submit"
+                            />
+                        </form>
+                    </div>
             </div>
         )
     }
