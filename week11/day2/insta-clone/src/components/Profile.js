9f37b45a90a7acda990b524c7414160d0f3d7179
randy chong <randychong27@yahoo.com>
2021-05-19 22:22:42 -0500

    added profile image, name, button, and settings

--- week11/day2/insta-clone/src/components/Profile.js
+++ week11/day2/insta-clone/src/components/Profile.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react'
+import Randy from "../images/randy.png"
+import Settings from "../images/settings.png"
 
 export default class Profile extends Component {
     render() {
         return (
             <div>
-                <h1>Profile</h1>
+                <p>
+                    <a>
+                        <img src={Randy} className="profile-image"></img>
+                    </a>
+                </p>
+
+                <p>
+                    randallchong
+                </p>
+
+                <button>Edit Profile</button>
+
+                <p>
+                    <img src={Settings} className="settings"></img>
+                </p>
             </div>
         )
     }
