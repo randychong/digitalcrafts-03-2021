31a39f71a1f534ec784ebbde886747d59c0ba7b7
randy chong <randychong27@yahoo.com>
2021-05-19 20:22:43 -0500

    created starter files for instagram clone

--- week11/day2/my-app/src/components/CardContainer.jsx
+++ week11/day2/my-app/src/components/CardContainer.jsx
@@ -2,89 +2,110 @@ import React, { Component } from 'react'
 import Card from "./Card"
 
 export default class CardContainer extends Component {
-    // constructor(props) {
-    //     super(props);
-    //     this.state = { pokemonName: '' };
-    //     this.state = { pokemonHP: '' };
-    //     this.state = { pokemonFront: '' };
-    //     this.state = { pokemonBack: '' };
-    //   }
-    //   myChangeHandler1 = (event) => {
-    //     this.setState({pokemonName: event.target.value});
-    //   }
-    //   myChangeHandler2 = (event) => {
-    //     this.setState({pokemonHP: event.target.value});
-    //   }
-    //   myChangeHandler3 = (event) => {
-    //     this.setState({pokemonFront: event.target.value});
-    //   }
-    //   myChangeHandler4 = (event) => {
-    //     this.setState({pokemonBack: event.target.value});
-    //   }
-
-      state = {
+    state = {
         searchCriteria: "",
-        pokemonBerries: [],
-        pokemon: {}
+        name: "",
+        id: "",
+        hp: "",
+        frontURL: "",
+        backURL: "",
+        newPokemon: []
     }
 
-    async componentDidMount() {
-      const berryUrl = "https://pokeapi.co/api/v2/berry/"
-       const fetchData = await fetch(berryUrl,{ headers: {
-      'Content-Type': 'application/json'
-      
-    },})
-    const json = await fetchData.json()
-    this.setState({
-        pokemonBerries:json.results
-    })
-   }
+    componentDidMount() {
+      this.setState({
+        newPokemon: this.props.pokemonData[0].pokemon
+      })
+      console.log(this.state.newPokemon)
+    }
 
     searchCriteria= (event) => {
         this.setState({
             searchCriteria: event.target.value
         })
     }
+
+    handleInputFormValues = (event) => {
+      this.setState({
+          [event.target.name]: event.target.value,
+      })
+  }
+
+    handleSubmit = (e) => {
+      const {name, id, hp, frontURL, backURL} = this.state
+      e.preventDefault();
+      const newPokemonData = {
+        name: name,
+        id: id,
+        hp: hp,
+        sprites: {
+          front: frontURL,
+          back: backURL,
+        }
+      }
+      this.setState({
+        newPokemon: [newPokemonData,...this.state.newPokemon]
+      })
+      console.log(newPokemonData)
+    }
     render() {
-        const {pokemonData} = this.props
-        const filteredData = pokemonData[0].pokemon.filter(pokemon => pokemon.name.includes(this.state.searchCriteria))
-        const newPokemon = {
-            id: 448,
-            name: "lucario",
-            hp: 70,
-            sprites: {
-              front:
-                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/448.png",
-              back: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/448.png",
-            },
-          }
+        const filteredData = this.state.newPokemon.filter(pokemon => pokemon.name.includes(this.state.searchCriteria))
         return (
             <div>
                 <h1>Card Container</h1>
                 <input onChange={(e)=>this.searchCriteria(e)}type="text" placeholder="Search for a pokemon"></input>
 
                 <div>
-                <form>
-                 <input className="create-field"type="text" placeholder="Enter a Name" onChange={this.myChangeHandler1}/>
-                 <input className="create-field" type="text" placeholder="Enter a HP" onChange={this.myChangeHandler2}/>
-                 <input className="create-field" type="text" placeholder="Enter front URL" onChange={this.myChangeHandler3}/>
-                 <input className="create-field" type="text" placeholder="Enter back URL" onChange={this.myChangeHandler4}/>
-                <button onClick={(e)=>{
-                    e.preventDefault()
-                    this.setState({pokemon: filteredData.push(newPokemon)})
-                    console.log(filteredData)
-                    }}className="create-button">Create</button>
+                <form
+                  onSubmit={this.handleSubmit}>
+                  <input 
+                    className="create-field"
+                    type="text" 
+                    placeholder="Enter a Name" 
+                    name="name" 
+                    value={this.state.name} 
+                    onChange={this.handleInputFormValues}
+                  />
+                  <input 
+                    className="create-field"
+                    type="text" 
+                    placeholder="Enter in ID" 
+                    name="id" 
+                    value={this.state.id} 
+                    onChange={this.handleInputFormValues}
+                  />
+                  <input 
+                    className="create-field" 
+                    type="text" 
+                    placeholder="Enter a HP" 
+                    name="hp" 
+                    value={this.state.hp} 
+                    onChange={this.handleInputFormValues}
+                  />
+                  <input 
+                    className="create-field" 
+                    type="text" 
+                    placeholder="Enter front URL" 
+                    name="frontURL" 
+                    value={this.state.frontURL} 
+                    onChange={this.handleInputFormValues}
+                  />
+                  <input 
+                    className="create-field" 
+                    type="text" 
+                    placeholder="Enter back URL" 
+                    name="backURL" 
+                    value={this.state.backURL}  
+                    onChange={this.handleInputFormValues}
+                  />
+                  <button className="create-button">Create</button>
                 </form>
                 </div>
 
                 <div className="card-container">
-                <p>{this.state.pokemonName}</p>
-                <p>{this.state.pokemonHP}</p>
-                <img src={this.state.pokemonFront}></img>
-                <img src={this.state.pokemonBack}></img>
                 {filteredData.map((singlePokemon,index) => <Card key={singlePokemon.name}  pokemon={singlePokemon}  />)}
                 </div>
             </div>
         )
-    }
+        }
 }
--- week11/day3/my-app/src/components/Form.js
+++ week11/day3/my-app/src/components/Form.js
@@ -13,58 +13,118 @@ export default class Form extends Component {
         country: "",
         phone: "",
     }
+
+    handleInputFormValues = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value,
+        })
+    }
+
+    handleSubmit = (e) => {
+        const {firstName,
+            lastName,
+            address,
+            apt,
+            postal,
+            city,
+            state,
+            country,
+            phone,
+        } = this.state
+        e.preventDefault();
+        const formSubmissionData = {
+            username: firstName + lastName,
+            address: address + "" + apt + "" + city + "" + state + "" + postal + "" + country + "",
+            phone: phone,
+        }
+    }
+
     render() {
         return (
             <div>
                 <h1>Forms React</h1>
                     <div className="shipping-container">
-                        <form className="shipping-form">
-                            <input type="text"
+                        <form
+                            className="shipping-form"
+                            onSubmit={this.handleSubmit}
+                            >
+                            <input 
+                                type="text"
                                 className="input-item"
-                                placeholder="First Name" 
+                                placeholder="First Name"
+                                name="firstName"
                                 value={this.state.firstName} 
-                                onChange={(e) =>
-                                    this.setState({
-                                    firstName: e.target.value
-                                    })
-                                }
+                                onChange={this.handleInputFormValues}
                             />
-                            <input type="text" 
-                            className="input-item" 
-                            placeholder="Last Name"
+                            <input 
+                                type="text" 
+                                className="input-item" 
+                                placeholder="Last Name"
+                                name="lastName"
+                                value={this.state.lastName}
+                                onChange={this.handleInputFormValues}
                             />
-                            <input type="text" 
-                            className="input-item" 
-                            id="street" placeholder="Street Address"
+                            <input 
+                                type="text" 
+                                className="input-item" 
+                                id="street" placeholder="Street Address"
+                                name="address"
+                                value={this.state.address}
+                                onChange={this.handleInputFormValues}
                             />
-                            <input type="text" 
-                            className="input-item" 
-                            id="apt" placeholder="Apt, Suite"
+                            <input 
+                                type="text" 
+                                className="input-item" 
+                                id="apt" placeholder="Apt, Suite"
+                                name="apt"
+                                value={this.state.apt}
+                                onChange={this.handleInputFormValues}
                             />
-                            <input type="text" 
-                            className="input-item" 
-                            placeholder="Postal Code"
+                            <input 
+                                type="text" 
+                                className="input-item" 
+                                placeholder="Postal Code"
+                                name="postal"
+                                value={this.state.postal}
+                                onChange={this.handleInputFormValues}
                             />
-                            <input type="text" 
-                            className="input-item" 
-                            placeholder="City"
+                            <input 
+                                type="text" 
+                                className="input-item" 
+                                placeholder="City"
+                                name="city"
+                                value={this.state.city}
+                                onChange={this.handleInputFormValues}
                             />
-                            <input type="text" 
-                            className="input-item" 
-                            placeholder="State"
+                            <input 
+                                type="text" 
+                                className="input-item" 
+                                placeholder="State"
+                                name="state"
+                                value={this.state.state}
+                                onChange={this.handleInputFormValues}
                             />
-                            <input type="text" 
-                            className="input-item" 
-                            placeholder="Country"
+                            <input 
+                                type="text" 
+                                className="input-item" 
+                                placeholder="Country"
+                                name="country"
+                                value={this.state.country}
+                                onChange={this.handleInputFormValues}
                             />
-                            <input type="text" 
-                            className="input-item" 
-                            placeholder="Phone"
+                            <input 
+                                type="text" 
+                                className="input-item" 
+                                placeholder="Phone"
+                                name="phone"
+                                value={this.state.phone}
+                                onChange={this.handleInputFormValues}
                             />
-                            <input type="submit" 
-                            className="input-item" 
-                            id="btn" 
-                            placeholder="Submit"
+                            <input
+                                type="submit" 
+                                className="input-item" 
+                                id="btn" 
+                                value="Submit"
                             />
                         </form>
                     </div>
