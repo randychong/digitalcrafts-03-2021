cb74ab6905e7567eb2f7f4fc4c60f7187fe84f92
randy chong <randychong27@yahoo.com>
2021-05-19 22:18:47 -0500

    created profile component

--- week11/day2/insta-clone/src/App.js
+++ week11/day2/insta-clone/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from "./components/Navbar"
+import Profile from "./components/Profile"
 
 function App() {
   return (
     <div className="App">
      <Navbar />
+     <Profile />
     </div>
   );
 }
