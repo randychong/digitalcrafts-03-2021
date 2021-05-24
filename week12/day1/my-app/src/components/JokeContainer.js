01c66991dd319dc12cb2208b8d451c6ba56cc611
randy chong <randychong27@yahoo.com>
2021-05-24 11:17:28 -0500

    added a useeffect to fetch api

--- week12/day1/my-app/src/components/JokeContainer.js
+++ week12/day1/my-app/src/components/JokeContainer.js
@@ -1,11 +1,37 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Joke from "../components/Joke"
 
 export default function JokeContainer() {
     const [joke, setJoke] = useState("")
+    const [name, setName] = useState("Joe")
+
+    // useEffect(() => {
+    //     console.log("hey")
+    // });
+
+    useEffect(() => {
+        getJoke()
+        console.log("hey you")
+    }, []);
+
+    // useEffect(() => {
+    //     getJoke()
+    //     console.log("hey you there")
+    // }, [name]);
+    
+    // useEffect(()=>{}) - similar to componentDidMount
+    const getJoke = async () => {
+        const response = await fetch("https://icanhazdadjoke.com", { headers: { Accept: "application/json" },
+        });
+        const json =  await response.json();
+        setJoke(json)
+    }
     return (
         <div>
             <h1>Joke Container</h1>
+            <p></p>
+            <button>Add</button>
+            <button>Subtract</button>
             <Joke />
         </div>
     )
