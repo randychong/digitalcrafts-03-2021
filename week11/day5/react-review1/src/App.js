897a872d3750486e4f958142973637fcf04d370f
randy chong <randychong27@yahoo.com>
2021-05-24 09:05:12 -0500

    created starter files for react app

---
+++ week11/day5/react-review1/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Greeter from "./components/Greeter"
+
+function App() {
+  return (
+    <div className="App">
+      <Greeter />
+    </div>
+  );
+}
+
+export default App;
---
+++ week11/day5/react-review1/src/components/Greeter.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react'
+
+export default function Greeter() {
+    const [greet, setGreet] = useState("")
+    return (
+        <div>
+            <h1>Greeter</h1>
+        </div>
+    )
+}
