8b02119925c8106cd04c4995a08069701f9312ea
Randy Chong <randychong27@yahoo.com>
2021-08-25 15:15:03 -0500

    updated title

--- week12/day1/movie-app/src/components/MovieContainer.js
+++ week12/day1/movie-app/src/components/MovieContainer.js
@@ -1,62 +1,70 @@
-import React, { useState, useEffect } from 'react'
-import Movie from "../components/Movie"
-import Key from "../Key"
+import React, { useState, useEffect } from "react";
+import Movie from "../components/Movie";
+import Key from "../Key";
 
 export default function MovieContainer() {
-    const [search, setSearch] = useState("")
-    const [movies, setMovies] = useState([])
+  const [search, setSearch] = useState("");
+  const [movies, setMovies] = useState([]);
 
-    useEffect(() => {
-        getFirstMovies()    
-    }, [])
+  useEffect(() => {
+    getFirstMovies();
+  }, []);
 
-    const getFirstMovies = async () => {
-        const response = await fetch(`http://www.omdbapi.com/?s=star+wars&apikey=${Key}`)
-        const json = await response.json();
-        setMovies(json.Search)
-    }
+  const getFirstMovies = async () => {
+    const response = await fetch(
+      `http://www.omdbapi.com/?s=star+wars&apikey=${Key}`
+    );
+    const json = await response.json();
+    setMovies(json.Search);
+  };
 
-    const getMovies = async () => {
-        const response = await fetch(`http://www.omdbapi.com/?s=${search}&apikey=${Key}`)
-        const json = await response.json();
-        setMovies(json.Search)
-        console.log(movies)
-    }
+  const getMovies = async () => {
+    const response = await fetch(
+      `http://www.omdbapi.com/?s=${search}&apikey=${Key}`
+    );
+    const json = await response.json();
+    setMovies(json.Search);
+    console.log(movies);
+  };
 
-    return (
-        <div>
-            <div className="search-container">
-                <h1 className="page-header">Show Stars</h1>
+  return (
+    <div>
+      <div className="search-container">
+        <h1 className="page-header">Media Search</h1>
 
-                <form>
-                    <input
-                        type="text"
-                        value={search}
-                        className="search"
-                        placeholder="movie name (ex. Star Wars)"
-                        onChange={(e) => {
-                            setSearch(e.target.value)
-                    }}></input>
-                    <button
-                        type="submit"
-                        className="search-button"
-                        onClick={(e) => {
-                            e.preventDefault()
-                            getMovies()
-                    }}>Search</button>
-                </form>
-            </div>
-            
-            <div className="movies-container">
-                {movies.map((movie) => (
-                    <Movie
-                    title={movie.Title} 
-                    poster={movie.Poster}
-                    year={movie.Year}
-                    id={movie.imdbID}
-                    />
-                ))}
-            </div>
-        </div>
-    )
+        <form>
+          <input
+            type="text"
+            value={search}
+            className="search"
+            placeholder="star wars, etc."
+            onChange={(e) => {
+              setSearch(e.target.value);
+            }}
+          ></input>
+          <button
+            type="submit"
+            className="search-button"
+            onClick={(e) => {
+              e.preventDefault();
+              getMovies();
+            }}
+          >
+            Search
+          </button>
+        </form>
+      </div>
+
+      <div className="movies-container">
+        {movies.map((movie) => (
+          <Movie
+            title={movie.Title}
+            poster={movie.Poster}
+            year={movie.Year}
+            id={movie.imdbID}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
---
+++ week3/day1/challenge.py
@@ -0,0 +1,38 @@
+def program(input):
+
+    list = []
+    input = str(input)
+    for x in input:
+        list.append(x)
+
+    # one = ["o", "n", "e"]
+
+    # for character in one:
+    #     one = one.replace(character, "")
+    # print(one)
+
+    if "z" in list and "e" in list and "r" in list and "o" in list:
+        print(0)
+    if "o" in list and "n" in list and "e" in list:
+        print(1)
+    if "t" in list and "w" in list and "o" in list:
+        print(2)
+    if "t" in list and "h" in list and "r" in list and "e" in list and "e" in list:
+        print(3)
+    if "f" in list and "o" in list and "u" in list and "r" in list:
+        print(4)
+    if "f" in list and "i" in list and "v" in list and "e" in list:
+        print(5)
+    if "s" in list and "i" in list and "x" in list:
+        print(6)
+    if "s" in list and "e" in list and "v" in list and "e" in list and "n" in list:
+        print(7)
+    if "e" in list and "i" in list and "g" in list and "h" in list and "t" in list:
+        print(8)
+    if "n" in list and "i" in list and "n" in list and "e" in list:
+        print(9)
+
+program("reuonnoinfe")
+# program("oeisowufxrzohgiettr")
+# program("veiifogvweesotwnetnvfeheiot")
+# program("xtneiootnrnoeneeeeuoeoheetehounzoiuetrhfefeezuivirfwieotgoottfnrnneghetserhrwsgesfherhtiitrerevreernhveo")
